b55095c47a3a13ac9992f0754c0c875eb8556c28
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2020-04-12 00:33:00 +0530

    Moving Towards 2nd Phase

--- reduxdemo/index.js
+++ reduxdemo/index.js
@@ -1,4 +1,37 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
-{
-    type:BUY_CAKE
+
+function buyCake(){
+    return {
+        type:BUY_CAKE,
+        info:"First Redux Action"
+    }
 }
+const initialState = {
+    numOfCakes:10
+}
+
+const reducer = (state=initialState,action)=>{
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes:state.numOfCakes - 1
+        }
+        default: return state;
+    }
+}
+
+const Store = createStore(reducer);
+console.log('Initial State',Store.getState());
+
+const unsubscribe = Store.subscribe(()=>{
+    console.log('updated State',Store.getState())
+});
+
+Store.dispatch(buyCake())
+Store.dispatch(buyCake())
+Store.dispatch(buyCake())
+unsubscribe();
